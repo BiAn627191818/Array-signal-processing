0e65a9c1d8e1b85dee3094dd337892be547598f9
shuaibin Bu <134117437+BiAn627191818@users.noreply.github.com>
2023-11-09 00:00:55 +0800

    Update music.m

--- DOA estimation/music.m
+++ DOA estimation/music.m
@@ -1,28 +1,26 @@
 clear all
 close all
-derad = pi/180;
-radeg = 180/pi;
+derad = pi/180;       %角度——>弧度
+radeg = 180/pi;        %弧度——>角度
 twpi = 2*pi;
-kelm = 8;
-dd = 0.5;
+kelm = 8;            %阵元数
+dd = 0.5;            %阵元间距
 d=0:dd:(kelm-1)*dd;
-iwave = 3;
-theta = [10 30 60];
-snr = 10;
-n = 500;
-A = exp(-j*twpi*d.'*sin(theta*derad));
-S = randn(iwave,n);
-X = A * S;
-X1 = awgn(X,snr,'measured');
-Rxx = X1*X1'/n;
+iwave = 3;            %信源数
+theta = [10 30 60];    %波达方向
+snr = 10;            %信噪比
+n = 500;            %采样数
+A = exp(-j*twpi*d.'*sin(theta*derad));    %方向向量
+S = randn(iwave,n);                        %信源信号
+X = A * S;                                %接收信号
+X1 = awgn(X,snr,'measured');               %添加噪声
+Rxx = X1*X1'/n;                            %计算协方差矩阵
 InvS=inv(Rxx);
-[EV,D]=eig(Rxx);
+[EV,D]=eig(Rxx);                            %特征值分解
 EVA=diag(D)'
-[EVA,I]=sort(EVA);
-EVA=diag(D)'
-[EVA,I]=sort(EVA);
-EVA=fliplr(EVA);
-EV=fliplr(EV(:,I));
+[EVA,I]=sort(EVA);                           %特征值从小到大排序
+EVA=fliplr(EVA);                               %左右翻转，从大到小排序
+EV=fliplr(EV(:,I));                            %对应特征向量排序
 
 %构造MUSIC空间谱函数
 for iang = 1:361
@@ -30,7 +28,7 @@ for iang = 1:361
     phim=derad*angle(iang);
     a=exp(-j*twpi*d*sin(phim)).';
     L=iwave;
-    En=EV(:,L+1:kelm);
+    En=EV(:,L+1:kelm);                        %得到噪声子空间
     SP(iang)=(a'*a)/(a'*En*En'*a);
 end
 
